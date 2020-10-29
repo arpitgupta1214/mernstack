81bb43f114e9f3f2bf1d81de1f0f5c3f3c45c333
Arpit Gupta <arpitgupta1214@gmail.com>
2020-10-29 11:14:47 +0530

    backend api changes

--- controllers/domainCtrl.js
+++ controllers/domainCtrl.js
@@ -3,19 +3,28 @@ const Domain = require("../models/domainModel");
 const domainCtrl = {
   getDomains: async (req, res) => {
     const keyword = req.query.keyword
-    ?{
-        domainName:{
-          $regex: req.query.keyword,
-          $options: "i",
+      ? {
+          domainName: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
         }
-        }
-    : {};
-    const pageSize = 10;
+      : {};
+    const pageSize = Number(req.query.pageSize) || 10;
     const page = Number(req.query.pageNumber) || 1;
+    const sort = req.query.sortField
+      ? {
+          [req.query.sortField]: req.query.sortDirection || 1,
+        }
+      : {};
     const count = await Domain.countDocuments({ ...keyword });
-    const domainLists = await Domain.find({ ...keyword })
-      .limit(pageSize)
-      .skip(pageSize * (page - 1));
+    // const domainLists = await Domain.find({ ...keyword })
+    const domainLists = await Domain.find({ ...keyword }, null, {
+      sort,
+      limit: pageSize,
+      skip: pageSize * (page - 1),
+    });
+
     return res.json({ domainLists, page, pages: Math.ceil(count / pageSize) });
   },
   createDomain: async (req, res) => {
@@ -32,6 +41,6 @@ const domainCtrl = {
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
-  }
+  },
 };
 module.exports = domainCtrl;
